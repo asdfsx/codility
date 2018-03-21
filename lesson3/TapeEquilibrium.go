d64aec0844ed359c8856bd12c1b6a2f727ef5884
Sun Xia <asdfsx@gmail.com>
2018-03-21 15:13:02 +0800

    udpate

--- lesson3/TapeEquilibrium.go
+++ lesson3/TapeEquilibrium.go
@@ -1,5 +1,36 @@
 package lesson3
 
-func Solution(A []int) int{
-	return 0
+import "fmt"
+
+func Solution(A []int) int {
+	cache := []int{}
+	countall := 0;
+	maxsize := 0;
+
+	if len(A) == 1{
+		return A[0]
+	}
+
+	for i, v := range A{
+		maxsize = i
+		countall += v
+		cache = append(cache, countall)
+		fmt.Println(countall, v)
+	}
+
+	result := 0;
+	for i, v := range cache{
+		if i == maxsize{
+			break
+		}
+		tmp := 2 * v - countall
+		if tmp >= 0 && (result > tmp || i == 0) {
+			result = tmp
+		} else if tmp < 0 && (result > -tmp || i == 0) {
+			result = -tmp
+		}
+		fmt.Println(result)
+	}
+
+	return result
 }
--- lesson3/TapeEquilibrium_test.go
+++ lesson3/TapeEquilibrium_test.go
@@ -5,5 +5,21 @@ import (
 )
 
 func TestSolution(t *testing.T) {
+ 	A:= []int{3,1,2,4,3}
+	result := Solution(A)
+	if result != 1{
+		t.Errorf("expect 1, but found %s", result)
+	}
 
+	A = []int{1000,1000}
+	result = Solution(A)
+	if result != 0{
+		t.Errorf("expect 0, but found %s", result)
+	}
+
+	A = []int{1,-1,1,-1}
+	result = Solution(A)
+	if result != 0{
+		t.Errorf("expect 0, but found %s", result)
+	}
 }
