a0aafbc928ae73f2c5d203e31680331f1197b6ac
asdfsx <asdfsx@gmail.com>
2018-03-23 00:12:20 +0800

    update (not perfect)

--- lesson4/MaxCounters.go
+++ lesson4/MaxCounters.go
@@ -1,1 +1,31 @@
 package lesson4
+
+func Solution4(N int, A []int) []int{
+	counter := make([]int, N)
+	currentmax := 0
+
+	for _, X := range A {
+		if X >= 1 && X <= N{
+			currentmax = increase(X, counter, currentmax)
+		} else if X == 1 + N {
+			maxcounter(currentmax, counter)
+		}
+	}
+
+	return counter
+}
+
+func increase(X int, counter []int, currentmax int) int {
+	counter[X-1]++
+	if counter[X-1] > currentmax{
+		return counter[X-1]
+	} else{
+		return currentmax
+	}
+}
+
+func maxcounter(max int, counter[]int){
+    for i, _ := range counter{
+    	counter[i] = max
+	}
+}
