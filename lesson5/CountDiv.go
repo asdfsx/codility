abe1cd151e420ee44540d18c9377f3247f6bc67c
Sun Xia <asdfsx@gmail.com>
2018-03-23 20:01:29 +0800

    update

---
+++ lesson5/CountDiv.go
@@ -0,0 +1,25 @@
+package lesson5
+
+import (
+	"fmt"
+	"github.com/docker/docker/pkg/sysinfo"
+)
+
+func Solution(A int, B int, K int) int {
+	if A == B && A % K == 0{
+		return 1
+	}
+	if K >= A && A != 0 {
+		A = 1
+	}
+	fmt.Println((B - A) / K)
+	if r := A % K; r == 0{
+		return (B - A) / K + 1
+	}
+	return (B - A) / K
+	//if r := A % K; r == 0{
+	//	return B/K - A/K +1
+	//} else {
+	//	return B/K - A/K +0
+	//}
+}
---
+++ lesson5/CountDiv_test.go
@@ -0,0 +1,27 @@
+package lesson5
+
+import (
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	result := Solution(6,11,2)
+	if result != 3{
+		t.Errorf("expect 3, but found %s", result)
+	}
+
+	result = Solution(11,345,17)
+	if result != 20{
+		t.Errorf("expect 20, but found %s", result)
+	}
+
+	result = Solution(0,0,11)
+	if result != 1{
+		t.Errorf("expect 1, but found %s", result)
+	}
+
+	result = Solution(10,10,5)
+	if result != 1{
+		t.Errorf("expect 1, but found %s", result)
+	}
+}
