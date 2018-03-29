eb8863ee014f6115227782d27d920ecbeae5e0ea
Sun Xia <asdfsx@gmail.com>
2018-03-29 18:17:08 +0800

    update

--- lesson7/Nesting.go
+++ lesson7/Nesting.go
@@ -1,1 +1,22 @@
 package lesson7
+
+func Solution4(S string) int{
+	stack := []int32{}
+	for _, v := range S{
+		if len(stack) == 0 {
+			stack = append(stack, v)
+		} else if len(stack)>0 && v == ')' && stack[len(stack)-1] =='(' {
+			stack = stack[0: len(stack)-1]
+		} else if len(stack)>1 && v == ')' && stack[len(stack)-2] =='(' {
+			stack = stack[0: len(stack)-2]
+		} else {
+			stack = append(stack, v)
+		}
+	}
+
+	if len(stack) == 0{
+		return 1
+	} else {
+		return 0
+	}
+}
--- lesson7/Nesting_test.go
+++ lesson7/Nesting_test.go
@@ -1,1 +1,17 @@
 package lesson7
+
+import (
+	"testing"
+)
+
+func TestSolution4(t *testing.T) {
+	result := Solution4("(()(())())")
+	if result != 1 {
+		t.Errorf("expect 1, but found %s", result)
+	}
+
+	result = Solution4("())")
+	if result != 0 {
+		t.Errorf("expect 0, but found %s", result)
+	}
+}
